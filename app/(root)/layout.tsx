fd7a9c6760a07ae46a456ad418d0792972750129
shahRhyme007 <arifurrahmanrhyme@gmail.com>
2024-06-20 20:56:50 -0500

    working on the sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,11 @@
+import Sidebar from '@/components/shared/Sidebar'
 import React from 'react'
+// this is where all the pages will be located
 
 const Layout = ({children}: {children: React.ReactNode}) => {
   return (
     <main className='root'>
+       <Sidebar/>
         <div className='root-container'>
             <div className="wrapper">
                 {children}
---
+++ components/shared/Sidebar.tsx
@@ -0,0 +1,19 @@
+
+import Link from 'next/link'
+import Image from 'next/image'
+
+const Sidebar = () => {
+  return (
+    <aside className='sidebar'>
+        <div className='flex size-full flex-col gap-4'>
+            <Link href='/' className='sidebar-logo'>
+                <Image src= '/assets/images/logo-text.png' alt='logo'
+                 width={180} height={28} />
+            </Link>
+        </div>
+    </aside>
+
+  )
+}
+
+export default Sidebar
