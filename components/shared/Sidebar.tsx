3841c29a36e485e927285cb298bb15a98c8c5fc1
shahRhyme007 <arifurrahmanrhyme@gmail.com>
2024-06-20 21:21:53 -0500

    TODO: add the signout on sibebar.tsx

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,8 +1,12 @@
-
+"use client"  // This is a client component instead of server component(which is the default)
 import Link from 'next/link'
 import Image from 'next/image'
+import { usePathname } from 'next/navigation'
+import { SignedIn } from '@clerk/nextjs'
+import { navLinks } from '@/constants'
 
 const Sidebar = () => {
+    const pathname = usePathname()
   return (
     <aside className='sidebar'>
         <div className='flex size-full flex-col gap-4'>
@@ -10,6 +14,34 @@ const Sidebar = () => {
                 <Image src= '/assets/images/logo-text.png' alt='logo'
                  width={180} height={28} />
             </Link>
+
+
+            {/* adding the navbars */}
+            <nav className='sidebar-nav'>
+                <SignedIn>
+                    <ul className='sidebar-nav_elements'>
+                        {navLinks.map((link) =>{
+                            const isActive = link.route === pathname
+
+                            return(
+                                <li key={link.route} className={`sidebar-nav_element group ${isActive ? "bg-purple-gradient text-white": "text-gray-500"}`}>
+                                    <Link className='sidebar-link flex items-center gap-2' href={link.route}>
+                                        <Image
+                                            src={link.icon} 
+                                            alt= "logo"
+                                            width={20} height={20}
+                                            className={`${isActive && 'brightness-200'}`}
+                                        />
+                                        {link.label}
+                                    </Link>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </SignedIn>
+
+                {/* FOR SIGNED OUT */}
+            </nav>
         </div>
     </aside>
 
