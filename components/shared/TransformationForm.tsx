b2112dbaf00697456996d1dbcd4f1a9e70e03b77
shahRhyme007 <arifurrahmanrhyme@gmail.com>
2024-06-25 22:50:48 -0500

    start doing cloudinary

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -51,7 +51,7 @@ const TransformationForm = ({action, data = null, userId, type, creditBalance, c
     const [transformationConfig, setTransformationConfig] = useState(config);
 
     const [isPending, startTransition] = useTransition()
-    const router = useRouter()
+    // const router = useRouter()
 
 
     const initialValues = data && action === "Update" ? {
@@ -127,80 +127,12 @@ const TransformationForm = ({action, data = null, userId, type, creditBalance, c
     defaultValues: initialValues,
   })
  
-
-  
   // 2. Define a submit handler.
-  async function onSubmit(values: z.infer<typeof formSchema>) {
-    setIsSubmitting(true);
-
-    if(data || image) {
-      const transformationUrl = getCldImageUrl({
-        width: image?.width,
-        height: image?.height,
-        src: image?.publicId,
-        ...transformationConfig
-      })
-
-      const imageData = {
-        title: values.title,
-        publicId: image?.publicId,
-        transformationType: type,
-        width: image?.width,
-        height: image?.height,
-        config: transformationConfig,
-        secureURL: image?.secureURL,
-        transformationURL: transformationUrl,
-        aspectRatio: values.aspectRatio,
-        prompt: values.prompt,
-        color: values.color,
-      }
-
-      if(action === 'Add') {
-        try {
-          const newImage = await addImage({
-            image: imageData,
-            userId,
-            path: '/'
-          })
-
-          if(newImage) {
-            form.reset()
-            setImage(data)
-            router.push(`/transformations/${newImage._id}`)
-          }
-        } catch (error) {
-          console.log(error);
-        }
-      }
-
-      if(action === 'Update') {
-        try {
-          const updatedImage = await updateImage({
-            image: {
-              ...imageData,
-              _id: data._id
-            },
-            userId,
-            path: `/transformations/${data._id}`
-          })
-
-          if(updatedImage) {
-            router.push(`/transformations/${updatedImage._id}`)
-          }
-        } catch (error) {
-          console.log(error);
-        }
-      }
-    }
-
-    setIsSubmitting(false)
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
   }
 
 
-
-
-
-  
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
