74e643be5ee5f85303f9b40008b9950f7bac579f
shahRhyme007 <arifurrahmanrhyme@gmail.com>
2024-06-25 20:40:29 -0500

    made the header and the subheader changing in each page

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,10 +1,13 @@
 import Header from '@/components/shared/Header'
 import React from 'react'
+import {transformationTypes} from '@/constants'
 
-const AddTrransformationTypePage = () => {
+const AddTrransformationTypePage = ({params : {type}} : SearchParamProps) => {
+  const transformation = transformationTypes[type]
+
   return (
-    <Header title="Transformation Title"
-        subtitle="Transformation Subtitle"/>
+    <Header title={transformation.title}
+        subtitle={transformation.subTitle}/>
 
 
   )
