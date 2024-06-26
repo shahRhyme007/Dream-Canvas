fc6ef52cf6f34761255dcc683df38c75f21680c9
shahRhyme007 <arifurrahmanrhyme@gmail.com>
2024-06-25 22:14:21 -0500

    working on the forms

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,13 +1,36 @@
 import Header from '@/components/shared/Header'
 import React from 'react'
 import {transformationTypes} from '@/constants'
+import TransformationForm from '@/components/shared/TransformationForm'
+import { auth } from '@clerk/nextjs/server'
+import { getUserById } from '@/lib/actions/user.actions'
+import { redirect } from 'next/navigation'
 
-const AddTrransformationTypePage = ({params : {type}} : SearchParamProps) => {
+const AddTrransformationTypePage = async({params : {type}} : SearchParamProps) => {
   const transformation = transformationTypes[type]
+  const {userId} = auth()
+
+  if (!userId) redirect('/sign-in')
+
+  const user  = await getUserById(userId)
 
   return (
-    <Header title={transformation.title}
+    <>
+     <Header title={transformation.title}
         subtitle={transformation.subTitle}/>
+
+      <TransformationForm
+        action='Add'
+        userId={user._id}
+        type = {transformation.type as TransformationTypeKey}
+        creditBalance={user.creditBalance}
+        
+        />
+    
+    </>
+
+
+   
 
 
   )
