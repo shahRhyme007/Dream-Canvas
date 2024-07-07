3bf23a8b7c77392d699cf8c8581736ab343b1a24
shahRhyme007 <arifurrahmanrhyme@gmail.com>
2024-07-07 16:38:23 -0500

    transformation page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,12 +4,17 @@ import { getAllImages } from "@/lib/actions/image.actions"
 import Image from "next/image"
 import Link from "next/link"
 
-const Home = () => {
+const Home = async ({ searchParams }: SearchParamProps) => {
+  const page = Number(searchParams?.page) || 1;
+  const searchQuery = (searchParams?.query as string) || '';
+
+  const images = await getAllImages({ page, searchQuery})
+
   return (
     <>
       <section className="home">
         <h1 className="home-heading">
-          Unleash Your Creative Vision with <br /> AI Image Editor
+          Unleash Your Creative Vision with Imaginify
         </h1>
         <ul className="flex-center w-full gap-20">
           {navLinks.slice(1, 5).map((link) => (
@@ -25,6 +30,15 @@ const Home = () => {
             </Link>
           ))}
         </ul>
+      </section>
+
+      <section className="sm:mt-12">
+        <Collection 
+          hasSearch={true}
+          images={images?.data}
+          totalPages={images?.totalPage}
+          page={page}
+        />
       </section>
     </>
   )
