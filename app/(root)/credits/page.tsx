630854052f5a0ebd9e321e0311156fd26f553f6e
shahRhyme007 <arifurrahmanrhyme@gmail.com>
2024-07-08 21:44:50 -0500

    note with checkout and profile page TODO: publish webiste

--- app/(root)/credits/page.tsx
+++ app/(root)/credits/page.tsx
@@ -7,7 +7,7 @@ import Header from "@/components/shared/Header";
 import { Button } from "@/components/ui/button";
 import { plans } from "@/constants";
 import { getUserById } from "@/lib/actions/user.actions";
-import Checkout from "@/components/shared/";
+import Checkout from "@/components/shared/Checkout";
 
 const Credits = async () => {
   const { userId } = auth();
